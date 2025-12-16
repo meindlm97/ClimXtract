e1fec223734e0a2bd483073ed56aeee22cb1f815
meindlm97 <58338204+meindlm97@users.noreply.github.com>
2025-12-16 20:27:10 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.1.2' 
+VERSION = '1.1.3' 
 DESCRIPTION = 'Climxtract'
 LONG_DESCRIPTION = 'ClimXtract: A Python Toolkit for Standardizing High-Resolution Climate Datasets for Regional Domains'
 
